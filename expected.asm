d97682a883d3b18c0eab535b73d17d90b4187d68
jhoanna.lagumbay <jtlagumbay@up.edu.ph>
2023-12-12 14:09:11 +0800

    Update expected.asm

--- expected.asm
+++ expected.asm
@@ -1,8 +1,61 @@
 .data
-  hello: .asciiz "Hello world!"
+  hello:  .asciiz "Hello world!"
+  new_line: .asciiz "\n"
+  result:      .word   0
+  a:      .space   128
+  b:      .word   2
+
 .text
-  la   $a0, hello        # load address of print heading
+
+main:
+  la   $a0, a           # address
+  la   $a1, 128         # size
+  li $v0, 8             # specify read integer service
+  syscall
+
+  # Initialize $s0 to 1 (assume it's an int)
+  li $s0, 0
+  move $a3, $a0
+  j check_type
+
+print_res:
+  addi   $a0, $s0, 0    # load address of print heading
+  li   $v0, 1           # specify Print integer service
+  syscall               # print heading
+  j exit
+  
+check_type:
+  # Load the first character from the buffer
+  lbu $s1, 0($a3)
+
+  jal print_char
+
+  beqz $s1, print_res
+
+  # Check if the first character is a digit
+  li $s2, 48     # ASCII code for '0'
+  li $s3, 57     # ASCII code for '9'
+  blt $s1, $s2, is_string
+  bgt $s1, $s3, is_string
+
+  # The first character is a digit so iterate
+  addi $a3, $a3, 1
+  j check_type
+
+is_string:
+  li $s0, 3
+  j print_res
+
+print_char:
+  move   $a0, $s1        # load address of print heading
+  li   $v0, 11           # specify Print String service
+  syscall                # print heading
+  
+  la   $a0, new_line    # load address of print heading
   li   $v0, 4           # specify Print String service
   syscall               # print heading
+  jr $ra
+
+exit:
   li   $v0, 10          # system call for exit
   syscall               # we are out of here.
