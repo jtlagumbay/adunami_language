f3ecd1d8e1d0bd577804f3278e1c62ba396f62ae
Princess Ethel <pmatillo@up.edu.ph>
2023-12-12 15:01:27 +0800

    Update expected.asm

--- expected.asm
+++ expected.asm
@@ -1,9 +1,9 @@
 .data
-  hello:  .asciiz "Hello world!"
-  new_line: .asciiz "\n"
-  result:      .word   0
-  a:      .space   128
-  b:      .word   2
+  hello:      .asciiz     "Hello world!"
+  new_line:   .asciiz     "\n"
+  result:     .word       0
+  a:          .space      128
+  b:          .word       2
 
 .text
 
@@ -13,8 +13,8 @@ main:
   li $v0, 8             # specify read integer service
   syscall
 
-  # Initialize $s0 to 1 (assume it's an int)
-  li $s0, 0
+  # Initialize $s0 to 1 (assume it's a string)
+  li $s0, 1
   move $a3, $a0
   j check_type
 
@@ -28,27 +28,25 @@ check_type:
   # Load the first character from the buffer
   lbu $s1, 0($a3)
 
-  jal print_char
+  # jal print_char
 
-  beqz $s1, print_res
+  beq $s1, 10, is_integer     # 32 -> ASCII code for ' '
 
   # Check if the first character is a digit
-  li $s2, 48     # ASCII code for '0'
-  li $s3, 57     # ASCII code for '9'
-  blt $s1, $s2, is_string
-  bgt $s1, $s3, is_string
+  blt $s1, 48, print_res      # 48 -> ASCII code for '0'
+  bgt $s1, 57, print_res      # 57 -> ASCII code for '9'
 
   # The first character is a digit so iterate
   addi $a3, $a3, 1
   j check_type
 
-is_string:
-  li $s0, 3
+is_integer:
+  li $s0, 0
   j print_res
 
 print_char:
   move   $a0, $s1        # load address of print heading
-  li   $v0, 11           # specify Print String service
+  li   $v0, 11           # specify Print Character service
   syscall                # print heading
   
   la   $a0, new_line    # load address of print heading
